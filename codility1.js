def9a4a35e9daeb1cdea330d3fb3637236819c3a
Tomasz Abramski <tomek.abramski@gmail.com>
2015-09-06 23:51:17 +0200

    Update codility1.js

--- codility1.js
+++ codility1.js
@@ -4,16 +4,13 @@ function solution(a){
 			var sum_first_a = a.slice(i).reduce(function(a,b){ return a+b});
 			var sum_second_a = a.slice(0, i).reduce(function(a,b){ return a+b});
 			var difference = Math.abs(sum_first_a - sum_second_a); 
-			
 			if(c==1){
 				min=difference;
 				c=2;
 			}
-			
 			if(min<difference){
 				min=difference;
 			}
-			
 			
 			i--;
 		}while(i>=1)
@@ -21,10 +18,8 @@ function solution(a){
 		return min;
 }
 
-var arr = new Array(10);
+var arr = new Array(100000);
 var l=arr.length-1;
 for(i=0; i<l; i++)
  arr[i] = Math.floor(Math.random()*2000)-1000;
-
-
 solution(arr);
